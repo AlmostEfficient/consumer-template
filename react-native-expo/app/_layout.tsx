adcc49805ec465de1becea62062cf102ee418b28
raza <42661870+AlmostEfficient@users.noreply.github.com>
2024-10-07 01:41:47 +1300

    using stock + animated splash

--- react-native-expo/app/_layout.tsx
+++ react-native-expo/app/_layout.tsx
@@ -1,64 +1,48 @@
 import { Stack } from "expo-router";
-import { Animated, Text, StyleSheet } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { magic } from "../config/magic";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect, useState, useCallback } from "react";
-import * as SplashScreen from "expo-splash-screen"
-import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { SplashScreen } from "../components/SplashScreen";
 
 export default function RootLayout() {
-	const [appIsReady, setAppIsReady] = useState(false);
 	const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
 
 	useEffect(() => {
-
-		async function prepare() {
-			// run any preload logic here: load fonts, make any API calls, etc.
+		async function checkFirstLaunch() {
 			try {
-				AsyncStorage.getItem('hasLaunchedBefore')
-					.then((value) => {
-						if (value === null) {
-							AsyncStorage.setItem('hasLaunchedBefore', 'true');
-							setIsFirstLaunch(true);
-						} else {
-							setIsFirstLaunch(false);
-						}
-					});
-				// artifical slow to test splash screen
-				await new Promise(resolve => setTimeout(resolve, 4000));
+				const value = await AsyncStorage.getItem('hasLaunchedBefore');
 
+				if (value === null) {
+					await AsyncStorage.setItem('hasLaunchedBefore', 'true');
+					setIsFirstLaunch(true);
+				} else {
+					setIsFirstLaunch(false);
+				}
 			} catch (error) {
 				console.error('Error checking first launch:', error);
-			} finally {
-				setAppIsReady(true);
+				setIsFirstLaunch(false);
 			}
 		}
 
-		prepare();
+		checkFirstLaunch();
 	}, []);
 
-	const onLayoutRootView = useCallback(async () => {
-		if (appIsReady) {
-			await SplashScreen.hideAsync();
-		}
-	}, [appIsReady]);
-
-	if (!appIsReady) {
-		return <Text style={{ fontSize: 34, fontWeight: 'bold', textAlign: 'center', marginTop: 100 }}>Loading...</Text>;
-	}
-
 	return (
-		<SafeAreaProvider>
-			<magic.Relayer />
-			<Stack>
-				{isFirstLaunch ? (
-					<Stack.Screen name="(onboarding)/index" options={{ headerShown: false }} />
-				) : (
-					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-				)}
-				<Stack.Screen name="+not-found" />
-			</Stack>
-		</SafeAreaProvider>
+		<SplashScreen>
+			{isFirstLaunch !== null && (
+				<SafeAreaProvider>
+					<magic.Relayer />
+					<Stack>
+						{isFirstLaunch ? (
+							<Stack.Screen name="(onboarding)/index" options={{ headerShown: false }} />
+						) : (
+							<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+						)}
+						<Stack.Screen name="+not-found" />
+					</Stack>
+				</SafeAreaProvider>
+			)}
+		</SplashScreen>
 	);
 }
